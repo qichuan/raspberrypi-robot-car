3307247fafa16db162e6c466e2bfeea1faaaac4c
qichuan <qichuan@zhangqichuan.com>
2014-11-28 22:25:15 +0800

    implemented go_forward

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -1,44 +1,52 @@
 import SimpleHTTPServer
 import SocketServer
 import urlparse
-# import motor
+from motor import Motor
 
 
 PORT = 8000
 
 class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    # def __init__(self):
-    # self.motor = Motor()
+    def __init__(self):
+        self.motor = Motor()
     def do_GET(self):
         parsedParams = urlparse.urlparse(self.path)
-        queryParsed = urlparse.parse_qs(parsedParams.query)
+        #queryParsed = urlparse.parse_qs(parsedParams.query)
         
         if parsedParams.path == "/forward":
-            self.go_forward(queryParsed)
+            self.go_forward()
+        elif parsedParams.path == "/backward":
+            self.go_backward()
+        elif parsedParams.path == "/turnRight":
+            self.turn_right()
+        elif parsedParams.path == "/turnLeft":
+            self.turn_left()
+
+    def go_forward(self):
+        self.motor.left_fwd_start()
+        self.motor.left_bck_stop()
+        self.motor.right_fwd_start()
+        self.motor.right_bck_stop()
+        self.print_msg("forward")
         
-        # request is either for a file to be served up or our test
-        if parsedParams.path == "/test":
-            self.processMyRequest(queryParsed)
-        else:
-            SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
-            
-    def processMyRequest(self, query):
+    def go_backward(self):
+        #self.motor.left_back_start();
+        #self.motor.right_bck_start();
+        self.print_msg("backward")
+        
+    def turn_left(self):
+        self.print_msg("turn left")
+        
+    def turn_right(self):
+        self.print_msg("turn right")
+        
+    def print_msg(self, msg):
         self.send_response(200)
-        self.send_header('Content-Type', 'application/xml')
+        self.send_header('Content-Type', 'text/plain')
         self.end_headers()
-        self.wfile.write("<?xml version='1.0'?>")
-        self.wfile.write("<sample>Some XML</sample>")
+        self.wfile.write(msg)
         self.wfile.close()
-
-    def go_forward(self, query):
-#        # self.motor.right_fwd_start()
-        self.send_response(200)
-        self.send_header('Content-Type', 'application/xml')
-        self.end_headers()
-        self.wfile.write("<?xml version='1.0'?>")
-        self.wfile.write("<command>forward</command>")
-        self.wfile.close()
-
+        
 Handler = MyHandler
 SocketServer.allow_reuse_address = True
 httpd = SocketServer.TCPServer(("", PORT), Handler)
--- raspberrypi/motor.py
+++ raspberrypi/motor.py
@@ -1,30 +1,44 @@
-import RPi.GPIO as GPIO
+from fake_rpi import FGPIO
+#import RPi.GPIO as GPIO
 import time
 
+
+GPIO = FGPIO()
+
+right_backward_pin=7
+right_forward_pin=11
+
+left_backward_pin=10
+left_forward_pin=12
+	
 class Motor:
-	right_backward_pin=7
-	right_forward_pin=11
 	
 	def __init__(self):
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setwarnings(False)
 	
-	def right_fwd_start():
+	def right_fwd_start(self):
 		GPIO.setup(right_forward_pin, GPIO.OUT)
-	#GPIO.setup(right_backward, GPIO.OUT)
-	#time.sleep(2)
-	def right_fwd_stop():
+	
+	def right_fwd_stop(self):
 		GPIO.setup(right_forward_pin, GPIO.IN);
 	
-	def right_bck_start():
+	def right_bck_start(self):
 		GPIO.setup(right_backward_pin, GPIO.OUT);
-	#time.sleep(2)
-	def right_back_stop():
+	
+	def right_bck_stop(self):
 		GPIO.setup(right_backward_pin, GPIO.IN);
 
-#GPIO.output(right_forward, True)
-#GPIO.output(right_backward, False)
-#time.sleep(2)
-#GPIO.output(right_forward, False)
-#GPIO.output(right_backward, False)
+	def left_fwd_start(self):
+		GPIO.setup(left_forward_pin, GPIO.OUT)
+	
+	def left_fwd_stop(self):
+		GPIO.setup(left_forward_pin, GPIO.IN);
+	
+	def left_bck_start(self):
+		GPIO.setup(left_backward_pin, GPIO.OUT);
+	
+	def left_bck_stop(self):
+		GPIO.setup(left_backward_pin, GPIO.IN);
+
 
